2d6a4180bed8e34010e5924c58efc4c4e2529dc1
Gihan Bandarage <gihan@Gihans-Laptop.local>
2025-07-25 16:00:56 +1200

    Fix ThemeToggle component and remove unused import

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,13 +1,8 @@
 "use client";
-import React, { useState } from 'react';
-
-
+import React from 'react';
 import { useTheme } from "next-themes";
 import { Button } from "./button";
-import { FaSun } from "react-icons/fa";
-import { FaMoon } from "react-icons/fa";
-import { FaS } from 'react-icons/fa6';
-
+import { FaSun, FaMoon } from "react-icons/fa";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -16,11 +11,11 @@ export function ThemeToggle() {
     <Button 
       variant="ghost" 
       size="icon"
-      className="rounded-full"
+      className="rounded-full relative"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <FaSun className='absolute h-5 w-5 rotate-0 scale-100 transition-all dark:- rotate-90 scale-0'></FaSun>
-      <FaMoon className='absolute h-5 w-5 rotate-90 scale-0 transition-all dark:- rotate-0 scale-100'></FaMoon>
-     </Button>
+      <FaSun className="absolute h-5 w-5 transition-all rotate-0 scale-100 dark:rotate-90 dark:scale-0" />
+      <FaMoon className="absolute h-5 w-5 transition-all rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+    </Button>
   );
 }
